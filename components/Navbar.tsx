2b7bb885fe3a607d6274f2cb4bd4a4deaa115ee0
saranskumar <saranskumarwh@gmail.com>
2025-09-25 12:26:55 +0530

    logos added 1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -152,7 +152,7 @@ export const Navbar = () => {
               }}
             >
               <button className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-700">
-                000000000000000000000000000000000000000000000000000000000000  <Image alt='logo' src='/logo2.png' width={100} height={80}></Image>
+              <Image alt='logo' src='/logo2.png' width={100} height={80}></Image>
             
               </button>
             </Link>
