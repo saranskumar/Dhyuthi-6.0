3f982fe901c781744bfb109b53c3088f84491478
saranskumar <saranskumarwh@gmail.com>
2025-09-25 23:14:11 +0530

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -152,7 +152,7 @@ export const Navbar = () => {
               }}
             >
               <button className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-700">
-              <Image alt='logo' src='/logo2.png' width={100} height={80}></Image>
+              <Image alt='logo' src='/logo2.png' width={80} height={60}></Image>
             
               </button>
             </Link>
