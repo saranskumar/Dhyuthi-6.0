3b0edd99ec2a144cd3c0a32906d7c10c13c13430
saranskumar <saranskumarwh@gmail.com>
2025-09-27 01:20:21 +0530

    heroali

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,23 +4,25 @@ import Image from 'next/image';
 const Hero = () => {
   return (
     <section id="page0" className="bg-transparent pt-44">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 ">
 
   <Image
     alt="logo"
     src="/dhyuthi.png"
       width={1360}
       height={322}
-    className="mx-auto"   // centers the logo horizontally
+    className="mx-auto  w-full shadow-xl flex flex-col p-4  rounded-lg
+                hover:scale-75 transform transition-transform duration-300 ease-in-out"   // centers the logo horizontally
   />
 
-  <a className="inline-flex justify-center items-center mt-8 py-1 px-1 pr-4 mb-7">
+  <a className="inline-flex justify-center items-center   px-1 pr-4 mb-7 w-full shadow-xl ">
     <Image
       alt="logo"
       src="/comingsoon.png"
-      width={680}
-    height={161}
-      className="mx-auto"  // centers the second image inside the link
+      width={453}
+    height={107}
+      className="mx-autow-full shadow-xl flex flex-col p-4 my-16 rounded-lg
+                hover:scale-103 transform transition-transform duration-300 ease-in-out"  // centers the second image inside the link
     />
   </a>
 
